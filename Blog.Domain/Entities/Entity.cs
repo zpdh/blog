9ddcaf04ea8086ec8a0b6b11c0c2935a419172a1
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-05 22:56:28 -0300

    feat: add initial migration

--- Blog.Domain/Entities/Entity.cs
+++ Blog.Domain/Entities/Entity.cs
@@ -2,5 +2,5 @@
 
 public abstract class Entity {
     public int Id { get; set; }
-    public DateTime CreationDate { get; set; }
+    public DateTime CreationDate { get; set; } = DateTime.UtcNow;
 }
