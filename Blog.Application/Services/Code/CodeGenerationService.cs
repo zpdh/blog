6c315c4266cb29610283cd0f9ef99668e655f669
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-07 22:44:38 -0300

    feat: service to generate unique code

---
+++ Blog.Application/Services/Code/CodeGenerationService.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Application.Services.Configs;
+using Blog.Domain.Repositories.Post;
+
+namespace Blog.Application.Services.Code;
+
+public interface ICodeGenerationService {
+    public string GenerateCode();
+}
+
+public class CodeGenerationService(
+    IPostReadRepository readRepository
+) : ICodeGenerationService {
+    private readonly Random _random = new();
+
+    public string GenerateCode() {
+        var charArray = new char[CodeGenerationConfiguration.NumberOfCharacters];
+        var maxIndex = CodeGenerationConfiguration.ValidCharacters.Length;
+
+        while (true) {
+            for (var i = 0; i < charArray.Length; i++) {
+                var charPosition = _random.Next(maxIndex);
+                var character = CodeGenerationConfiguration.ValidCharacters[charPosition];
+
+                charArray[i] = character;
+            }
+
+            var code = new string(charArray);
+
+            if (readRepository.PostWithCodeExists(code)) {
+                continue;
+            }
+
+            return code;
+        }
+    }
+}
---
+++ Blog.Application/Services/Configs/CodeGenerationConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Application.Services.Configs;
+
+public class CodeGenerationConfiguration {
+    public const int NumberOfCharacters = 8;
+    public const string ValidCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+}
