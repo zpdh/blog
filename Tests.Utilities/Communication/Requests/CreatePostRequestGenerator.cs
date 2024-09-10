92bf61be6bb58fc75712e5e0d825986f5482c8a1
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-10 06:04:56 -0300

    feat: add generator for create post request

---
+++ Tests.Utilities/Communication/Requests/CreatePostRequestGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Domain.Communication.Requests.Post;
+using Bogus;
+using Moq;
+
+namespace Tests.Utilities.Communication.Requests;
+
+public class CreatePostRequestGenerator {
+    public static CreatePostRequest Generate() {
+        return new Faker<CreatePostRequest>()
+            .RuleFor(post => post.Title, faker => faker.Lorem.Word())
+            .RuleFor(post => post.TextContent, faker => faker.Lorem.Text());
+    }
+}
