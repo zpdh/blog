688557c344a2ab46693acd2fcd1a75863ba581f0
Antunes <36316038+zpdh@users.noreply.github.com>
2024-09-17 05:15:43 -0300

    tests: add get user integration test

---
+++ Tests.API/User/Get/GetUserTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using FluentAssertions;
+using Tests.Utilities.Services;
+
+namespace Tests.API.User.Get;
+
+public class GetUserTests : BlogClassFixture {
+    private readonly HttpMethod _method = HttpMethod.Get;
+    private const string Endpoint = "api/user/get";
+    private readonly Blog.Domain.Entities.User _user;
+
+    public GetUserTests(BlogWebApplicationFactory factory)
+        : base(factory) {
+        _user = factory.User;
+    }
+
+    [Fact]
+    public async Task Success() {
+        var tokenGenerator = TokenGeneratorBuilder.Build();
+        var token = tokenGenerator.Generate(_user.Id);
+
+        var response = await SendRequestAsync(_method, Endpoint, token: token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseContent = await ParseResponseAsync(response);
+
+        responseContent.GetProperty("username").GetString().Should().Be(_user.Username);
+        responseContent.GetProperty("email").GetString().Should().Be(_user.Email);
+    }
+
+    [Fact]
+    public async Task InvalidTokenError() {
+        var tokenGenerator = TokenGeneratorBuilder.Build();
+        var token = tokenGenerator.Generate(Guid.NewGuid());
+
+        var response = await SendRequestAsync(_method, Endpoint, token: token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
